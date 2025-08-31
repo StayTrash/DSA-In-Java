ec5b93e0ad6a3404705f13a3e43f0e440ea356a7
StayTrash <shubham.2022ug2012@iiitranchi.ac.in>
2025-09-01 01:16:07 +0530

    stack & queues problems added

---
+++ 24-Stacks&Queues/Ques/src/Q2LargestAreaHistogram.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+import java.util.Stack;
+
+class Q2LargestAreaHistogram {
+    public int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+
+        int max = 0;
+        stack.push(0);
+
+        for (int i = 1; i < heights.length; i++) {
+            while (!stack.isEmpty() && heights[i] < heights[stack.peek()]) {
+                max = getMax(heights, stack, max, i);
+            }
+
+            stack.push(i);
+        }
+
+        int i = heights.length;
+
+        while (!stack.empty()) {
+            max = getMax(heights, stack, max, i);
+        }
+
+        return max;
+    }
+
+    private static int getMax(int[] arr, Stack<Integer> stack, int max, int i) {
+        int area;
+        int popped = stack.pop();
+
+        if (stack.empty()) {
+            area = arr[popped] * i;
+        } else {
+            area = arr[popped] * (i - 1 - stack.peek());
+        }
+
+        return Math.max(max, area);
+    }
+}
---
+++ 24-Stacks&Queues/Ques/src/Q4MinAddToMakeValidParenthesis.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+import java.util.Stack;
+
+class Q4MinAddToMakeValidParenthesis {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+        return stack.size();
+    }
+}
